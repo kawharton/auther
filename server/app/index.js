2e2c8ce1509e89891821fd22a09156b0d7c51c5a
oceanwaves19 <Khadija@Khadijas-MacBook-Pro.local>
2016-02-16 14:35:57 -0500

    initial commit

---
+++ server/app/index.js
@@ -0,0 +1,38 @@
+'use strict'; 
+
+var app = require('express')();
+var path = require('path');
+var session = require('express-session');
+
+app.use(session({
+    // this mandatory configuration ensures that session IDs are not predictable
+    secret: 'tongiscool'
+}));
+
+app.use(function (req, res, next) {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter', ++req.session.counter);
+  next();
+});
+
+app.use(require('./logging.middleware'));
+
+app.use(require('./requestState.middleware'));
+
+app.use(require('./statics.middleware'));
+
+
+
+app.use('/api', require('../api/api.router'));
+
+var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
+var indexPath = path.join(__dirname, '..', '..', 'public', 'index.html');
+validFrontendRoutes.forEach(function (stateRoute) {
+	app.get(stateRoute, function (req, res) {
+		res.sendFile(indexPath);
+	});
+});
+
+app.use(require('./error.middleware'));
+
+module.exports = app;
